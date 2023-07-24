545447033348a31e717a173b221a5955a30f8087
iliyaahmadi <iliyahmdi@yahoo.com>
2023-07-24 21:15:37 +0330

    fix : edit tables

--- TablesQuery.sql
+++ TablesQuery.sql
@@ -4,18 +4,24 @@ create table Roles(
 );
 
 create table Users(
-	ID BIGSERIAL NOT NULL PRIMARY KEY,
+	ID VARCHAR(50) NOT NULL PRIMARY KEY,
 	User_Name VARCHAR(50) NOT NULL,
+	User_Password VARCHAR(50) NOT NULL,
 	User_Email VARCHAR(50) NOT NULL,
 	User_Role_ID BIGINT REFERENCES Roles (ID),
 	User_Image BYTEA
 );
 
 create table Artists(
-	ID BIGSERIAL NOT NULL PRIMARY KEY,
+	ID VARCHAR(50) NOT NULL PRIMARY KEY,
 	Artist_Name VARCHAR(50),
 	Artist_Image BYTEA,
-    User_ID BIGINT REFERENCES Users (ID)
+	User_ID VARCHAR(50) REFERENCES Users (ID)
+);
+
+create table Followers (
+	User_ID VARCHAR(50) REFERENCES Users (ID),
+	Artist_ID VARCHAR(50) REFERENCES Artists (ID)
 );
 
 create table Genres(
@@ -23,59 +29,53 @@ create table Genres(
 	Genre_Name VARCHAR(50) NOT NULL
 );
 
-
 CREATE TABLE Albums (
-	ID BIGSERIAL NOT NULL PRIMARY KEY,
+	ID VARCHAR(50) NOT NULL PRIMARY KEY,
 	Name VARCHAR(50) NOT NULL,
-	Artist_ID BIGINT REFERENCES Artists (ID),
+	Artist_ID VARCHAR(50) REFERENCES Artists (ID),
 	Release_Date DATE,
 	Cover BYTEA
 );
 
-create table Songs(
-	ID BIGSERIAL NOT NULL PRIMARY KEY,
-	Song_Title VARCHAR(50) NOT NULL,
+create table Tracks(
+	ID VARCHAR(50) NOT NULL PRIMARY KEY,
+	Track_Title VARCHAR(50) NOT NULL,
 	Likes BIGINT NOT NULL,
 	Duration INT NOT NULL,
 	Path VARCHAR(250) NOT NULL,
 	CoverUrl BYTEA,
-	Song_Artist BIGINT REFERENCES Artists (ID),
-	Song_Album BIGINT REFERENCES Albums (ID)
+	Track_Artist VARCHAR(50) REFERENCES Artists (ID),
+	Track_Album VARCHAR(50) REFERENCES Albums (ID)
 );
 
 create table Playlist(
-	ID BIGSERIAL NOT NULL PRIMARY KEY,
+	ID VARCHAR(50) NOT NULL PRIMARY KEY,
 	Playlist_Name VARCHAR(50) NOT NULL,
-	User_ID BIGINT REFERENCES Users(ID)
+	User_ID VARCHAR(50) REFERENCES Users(ID)
 );
 
-create table Song_Genre(
+create table Track_Genre(
 	Genre_ID BIGINT REFERENCES Genres(ID),
-	Song_ID BIGINT REFERENCES Songs (ID)
+	Track_ID VARCHAR(50) REFERENCES Tracks (ID)
 );
 
-create table Playlist_Song(
-	Playlist_ID BIGINT REFERENCES Playlist (ID),
-	Song_ID BIGINT REFERENCES Songs (ID)
+create table Playlist_Track(
+	Playlist_ID VARCHAR(50) REFERENCES Playlist (ID),
+	Track_ID VARCHAR(50) REFERENCES Tracks (ID)
 );
 
 create table Popular(
-	Song_ID BIGINT REFERENCES Songs (ID),
-	UNIQUE(Song_ID)
+	Track_ID VARCHAR(50) REFERENCES Tracks (ID),
+	UNIQUE(Track_ID)
 );
 
 create table Likes(
 	Like_Date_Time date,
-	User_ID BIGINT REFERENCES Users (ID),
-	Song_ID BIGINT REFERENCES Songs (ID)
+	User_ID VARCHAR(50) REFERENCES Users (ID),
+	Track_ID VARCHAR(50) REFERENCES Tracks (ID)
 );
 
 create table History(
-	User_ID BIGINT REFERENCES Users (ID),
-	Song_ID BIGINT REFERENCES Songs (ID)
-);
-
-create table Followers (
-	User_ID BIGINT REFERENCES Users (ID),
-	Artist_ID BIGINT REFERENCES Artists (ID)
+	User_ID VARCHAR(50) REFERENCES Users (ID),
+	Track_ID VARCHAR(50) REFERENCES Tracks (ID)
 );
